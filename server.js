b1c698fa90c12c7ad1eb9a185a48fd936c5af2af
Devin Piner <devpindroid@gmail.com>
2020-04-04 17:06:07 -0600

    testing aggregate

--- server.js
+++ server.js
@@ -131,6 +131,9 @@ router.get('/movies', authJwtController.isAuthenticated, function(req, res) {
                                     $lookup:
                                         {
                                             from: "reviews",
+                                            pipeline: [
+                                                {$match: {title: query.title}}
+                                            ],
                                             localField: "title",
                                             foreignField: "movietitle",
                                             as: "movie_reviews"
